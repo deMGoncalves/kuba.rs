20f5382685246c542aa7f36384b43b13dadb9be4
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-23 15:52:05 -0300

    chore(f): adds two values

--- src/f.rs
+++ src/f.rs
@@ -1,5 +1,13 @@
-#[test]
-fn it_add() {
-    let result = 2 + 2;
-    assert_eq!(result, 4);
+/// Adds two values
+///
+/// # Examples
+///
+/// ```
+/// use kuba::f;
+///
+/// let result = f::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
