adf5838036d33814020d58800b3c76c15324c180
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-23 16:10:20 -0300

    chore(f): Divide two values

--- src/f.rs
+++ src/f.rs
@@ -11,3 +11,31 @@
 pub fn add(x: i32, y: i32) -> i32 {
     x + y
 }
+
+/// Divides two numbers
+///
+/// # Examples
+///
+/// ```
+/// use kuba::f;
+///
+/// let result = f::divide(3, 2);
+/// assert_eq!(result, 1);
+/// ```
+///
+/// # Panic
+///
+/// The function panics if the second argument is zero
+///
+/// ```rust,should_panic
+/// use kuba::f;
+///
+/// let result = f::divide(3, 0);
+/// ```
+pub fn divide(x: i32, y: i32) -> i32 {
+    if x == 0 {
+        panic!("Divide by zero");
+    }
+
+    x / y
+}
