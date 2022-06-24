7320c513cee667e4ef75c09056084f104b8eeafe
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-24 15:56:32 -0300

    chore(f): subtracts its second argument from its first argument

--- standard/src/f/mod.rs
+++ standard/src/f/mod.rs
@@ -1,8 +1,12 @@
 pub mod add;
 pub mod divide;
+pub mod subtract;
 
 #[doc(inline)]
 pub use self::add::add;
 
 #[doc(inline)]
 pub use self::divide::divide;
+
+#[doc(inline)]
+pub use self::subtract::subtract;
---
+++ standard/src/f/subtract.rs
@@ -0,0 +1,13 @@
+/// Subtracts its second argument from its first argument
+///
+/// # Examples
+///
+/// ```
+/// use standard::f;
+///
+/// let result = f::subtract(3, 2);
+/// assert_eq!(result, 1);
+/// ```
+pub fn subtract (x: i32, y: i32) -> i32 {
+    x - y
+}
